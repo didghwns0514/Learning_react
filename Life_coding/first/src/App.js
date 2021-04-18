cf6051329f78aedf1b6a5dbf34cf85995b91514b
Hhhh Jjjj <didghwns0514@gmail.com>
2021-04-18 19:31:01 +0900

    2021.04.18

--- Life_coding/first/src/App.js
+++ Life_coding/first/src/App.js
@@ -1,6 +1,45 @@
-// import logo from './logo.svg';
-import React, { Component } from 'react';
+import React, { Component } from 'react'; // eslint-disable-next-line
+import { render } from 'react-dom'; // eslint-disable-next-line
 import './App.css';
+
+
+
+
+class TOC extends Component {
+  render() {
+    return (
+        <nav>
+          <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JavaScript</a></li>
+          </ul>
+        </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render(){
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
 
 
 /// 이부분이 고쳐져야 작동!
@@ -8,7 +47,17 @@ class App extends Component{
   render() {
     return(
       <div className="App">
-        Hello react!
+        {/* 이 부분에 위에서 추가한 subject를 넣어서
+        render 하게 함. 사용자 component 추가 */}
+        <Subject 
+          title="WEB"
+          sub="new World Wide Web with React!">
+        </Subject> 
+        <TOC></TOC>
+        <Content
+          title="HTML"
+          desc="HTML is a HyperText Markup Language!">
+        </Content>
       </div>
     )
   }
